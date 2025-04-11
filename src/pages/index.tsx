78c19782f291eb128d238e22a0240804ed887aa9
sonyugwan <yugwan.son@epapyrus.com>
2025-04-11 09:36:19 +0900

    123123

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   
   useEffect(() => {
     setTimeout(() => {
-      // @ts-expect-error 123
+      // @ts-expect-error 12321312
       initMuPDFWebViewer('#viewer', 'https://static-test.hq.epapyrus.com/doc/sample.pdf', { licenseKey: '5z9yalNzmu1f+ta2ta2a6ja4' });
     }, 1000);
   }, []);
